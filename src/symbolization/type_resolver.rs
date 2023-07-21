3cea7614dae62719c6024db279d9780dd29c80a1
Geert van Ieperen <gvanieperen@greefa.nl>
2023-07-21 15:20:56 +0200

    type resolution mutator progress

--- src/symbolization/type_resolver.rs
+++ src/symbolization/type_resolver.rs
@@ -1,4 +1,4 @@
-use std::rc::Rc;
+use std::{rc::Rc, borrow::BorrowMut};
 
 use simple_error::SimpleError;
 
@@ -9,21 +9,21 @@ pub struct TypeResolver {
 }
 
 impl TypeResolver {
-    pub fn resolve_scope(&self, scope: &mut Scope) -> Result<&Scope, SimpleError> {
-        for (name, type_def) in &scope.types {
-            **type_def = self.resolve_type(**type_def, scope)?;
+    pub fn resolve_scope(&self, scope: &mut Scope) -> Result<(), SimpleError> {
+        for (_type_name, mut type_def) in &scope.types {
+            self.resolve_type(type_def.borrow_mut(), scope)?;
         }
 
-        for (subscope_name, subscope) in &mut scope.scopes {
+        for (_subscope_name, subscope) in &mut scope.scopes {
             self.resolve_scope(subscope)?;
         }
 
-        Ok(scope)
+        Ok(())
     }
 
     fn resolve_scope_reference<'a>(
         &'a self,
-        scope_to_resolve: Vec<Identifier>,
+        scope_to_resolve: &[Identifier],
         current: &'a Scope,
     ) -> Result<&'a Scope, SimpleError> {
         match scope_to_resolve.first() {
@@ -52,105 +52,88 @@ impl TypeResolver {
             None => Ok(current),
             Some(first) => {
                 if let Some(child_scope) = current.scopes.get(first) {
-                    return self.resolve_local_scope_reference(&scope[1..], child_scope);
-                };
-                Err(SimpleError::new(format!(
-                    "Could not find '{first}' in scope {:?}",
-                    current.full_name
-                )))
+                    self.resolve_local_scope_reference(&scope[1..], child_scope)
+                }
+                else
+                {
+                    Err(SimpleError::new(format!(
+                        "Could not find '{first}' in scope {:?}",
+                        current.full_name
+                    )))
+                }
             }
         }
     }
 
     fn resolve_type(
         &self,
-        type_to_resolve: TypeDefinition,
+        type_to_resolve: &mut TypeDefinition,
         local_scope: &Scope,
-    ) -> Result<TypeDefinition, SimpleError> {
-        match type_to_resolve.sub_type {
-            TypeSubType::Base { derived: None } => Ok(type_to_resolve),
-            TypeSubType::Enum { .. } => Ok(type_to_resolve),
-            TypeSubType::Base {
-                derived: Some(derived),
-            } => {
-                let resolved_type_ref = self.resolve_type_ref(
-                    *derived,
+    ) -> Result<(), SimpleError> {
+        match &mut type_to_resolve.sub_type {
+            TypeSubType::Base { derived: None } => {},
+            TypeSubType::Enum { .. } => {},
+            TypeSubType::Base { derived: Some(to_resolve), } => {
+                self.resolve_type_ref(
+                    to_resolve.borrow_mut(),
                     &type_to_resolve.generic_parameters,
                     local_scope,
                 )?;
-
-                Ok(TypeDefinition {
-                    name: type_to_resolve.name,
-                    generic_parameters: type_to_resolve.generic_parameters,
-                    sub_type: TypeSubType::Base {
-                        derived: Some(Box::from(resolved_type_ref)),
-                    },
-                })
             }
-            TypeSubType::Variant { mut variants } => {
-                for variant in &mut variants {
-                    let resolved_value_type = self.resolve_type_ref(
-                        variant.value_type,
+            TypeSubType::Variant { variants } => {
+                for variant in variants {
+                    self.resolve_type_ref(
+                        &mut variant.value_type,
                         &type_to_resolve.generic_parameters,
                         local_scope,
                     )?;
-                    variant.value_type = resolved_value_type;
                 }
-
-                Ok(TypeDefinition {
-                    name: type_to_resolve.name,
-                    generic_parameters: type_to_resolve.generic_parameters,
-                    sub_type: TypeSubType::Variant { variants },
-                })
             }
-            TypeSubType::Tuple { mut elements } => {
-                for elt in &mut elements {
-                    let resolved_value_type = self.resolve_type_ref(
-                        *elt,
+            TypeSubType::Tuple { elements } => {
+                for elt in elements {
+                    self.resolve_type_ref(
+                        elt,
                         &type_to_resolve.generic_parameters,
                         local_scope,
                     )?;
-                    *elt = resolved_value_type;
                 }
-
-                Ok(TypeDefinition {
-                    name: type_to_resolve.name,
-                    generic_parameters: type_to_resolve.generic_parameters,
-                    sub_type: TypeSubType::Tuple { elements },
-                })
             }
         }
+
+        Ok(())
     }
 
     fn resolve_type_ref(
         &self,
-        type_to_resolve: TypeRef,
+        type_to_resolve: &mut TypeRef,
         generics: &[Rc<GenericParameter>],
         local_scope: &Scope,
-    ) -> Result<TypeRef, SimpleError> {
+    ) -> Result<(), SimpleError> {
         match type_to_resolve {
             TypeRef::UnresolvedName(name_to_resolve) => {
                 let defined_type_ref =
                     self.resolve_defined_name(name_to_resolve, generics, local_scope)?;
-                Ok(TypeRef::Defined(defined_type_ref))
+                *type_to_resolve = TypeRef::Defined(defined_type_ref);
             }
-            _ => Ok(type_to_resolve),
+            _ => {},
         }
+        Ok(())
     }
 
     fn resolve_defined_name(
         &self,
-        type_to_resolve: UnresolvedName,
+        type_to_resolve: &UnresolvedName,
         generics: &[Rc<GenericParameter>],
         local_scope: &Scope,
     ) -> Result<DefinedTypeRef, SimpleError> {
         let mut generic_parameters = Vec::new();
-        for ele in type_to_resolve.generic_parameters {
-            let ele_ref = self.resolve_type_ref(ele, generics, local_scope)?;
+        for ele in &type_to_resolve.generic_parameters {
+            let mut ele_ref = ele.clone();
+            self.resolve_type_ref(&mut ele_ref, generics, local_scope)?;
             generic_parameters.push(ele_ref);
         }
 
-        let resolved_scope = self.resolve_scope_reference(type_to_resolve.scope, local_scope)?;
+        let resolved_scope = self.resolve_scope_reference(&type_to_resolve.scope, local_scope)?;
         let resolved_type = resolved_scope
             .types
             .get(&type_to_resolve.name)
