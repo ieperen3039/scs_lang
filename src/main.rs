136bd5c7d34ef4b39cbaefcac5690b667716a151
Geert van Ieperen <ieperen3039@outlook.com>
2023-07-26 12:35:50 +0200

    introduce clap to parse command line

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,54 @@
 
 
+use std::path::PathBuf;
+
 #[allow(dead_code)]
-use crate::commandline_parser::ArgumentParser;
 use crate::compiler::FauxCompiler;
 
 pub mod compiler;
 
-mod commandline_parser;
 mod parsing;
 mod symbolization;
 
 #[cfg(test)]
 mod tests;
 
-fn main() {
-    let mut arg_parser = ArgumentParser::new();
-    
-    let base_directory = std::env::current_dir().expect("Could not get this program's current directory");
+use clap::Parser;
 
-    let definition = if let Some(file_name) = arg_parser.get_parameter("--definition") {
-        let file_contents = std::fs::read_to_string(&file_name);
+/// Compiler for the Faux programming language
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct CommandlineArguments {
+    /// Path to the ebnf definition file to use when parsing
+    #[arg(short, long, value_name = ".ebnf file")]
+    definition: PathBuf,
+    /// Path to the file to compile
+    #[arg(short, long, value_name = ".faux file")]
+    program: PathBuf,
+    /// Optional path to a debug xml output
+    #[arg(long)]
+    xml: Option<PathBuf>,
+}
+
+fn main() {
+    let args = CommandlineArguments::parse();
+
+    let definition = {
+        let file_contents = std::fs::read_to_string(&args.definition);
         if file_contents.is_err() {
-            panic!("File {file_name} can not be opened")
+            panic!("File {} can not be opened", args.definition.display())
         } else {
             file_contents.unwrap()
         }
-    } else {
-        panic!("missing --definition tag");
     };
 
-    let program_file = arg_parser.get_parameter("--program").expect("missing --program tag");
+    let base_directory = std::env::current_dir().expect("Could not get this program's current directory");
+    let program_file = base_directory.join(args.program);
 
-    let xml_output_file = arg_parser.get_parameter("--xml").map(std::fs::File::create).map(Result::ok).flatten();
+    let xml_output_file = args.xml.map(std::fs::File::create).map(Result::ok).flatten();
 
     let mut compiler = FauxCompiler::build(&definition, xml_output_file).unwrap();
-    let compile_result = compiler.compile(&base_directory.join(program_file));
+    let compile_result = compiler.compile(&program_file);
     match compile_result {
         Ok(program) => print!("{}", program.name),
         Err(simple_error) => print!("{}", simple_error),
