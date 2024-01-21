755407711232a1148534e4ea3824a75cf19f5fed
Geert van Ieperen <ieperen3039@outlook.com>
2024-01-21 20:49:01 +0100

    failed attempt at TermBottomUpIter

--- src/transforming/grammar_util.rs
+++ src/transforming/grammar_util.rs
@@ -1,45 +1,113 @@
+use std::collections::HashMap;
+
 use crate::parsing::token::Token;
 
 use super::grammar::*;
 
-pub fn transform_terminals<Transformation>(term: &mut Term, transformation: &Transformation)
-where
-    Transformation: Fn(Term) -> Term,
-{
-    match term {
-        Term::Concatenation(terms) => {
-            for t in terms {
-                transform_terminals(t, transformation);
-            }
-        }
-        Term::Alternation(terms) => {
-            for t in terms {
-                transform_terminals(t, transformation);
-            }
-        }
-        _ => *term = transformation(term.clone()),
-    };
+struct TermBottomUpIter<'c> {
+    node: Option<&'c mut Term>,
+    internal: Option<Box<TermBottomUpIter<'c>>>,
+    sub_iter: std::slice::IterMut<'c, Term>,
 }
 
-pub fn apply_recursively<TermReader>(term: &Term, function: &TermReader)
-where
-    TermReader: Fn(&Term),
-{
-    function(term);
+impl<'c> TermBottomUpIter<'c> {
+    fn new(target: &'c mut Term) -> TermBottomUpIter<'c> {
+        match target {
+            Term::Concatenation(ts) | Term::Alternation(ts) => {
+                let mut sub_iter = ts.iter_mut();
+                let first = sub_iter
+                    .next()
+                    .expect("Concatenations and Alterations may never be empty");
+                let internal = Some(Box::new(TermBottomUpIter::new(first)));
+                TermBottomUpIter {
+                    node: Some(target),
+                    internal,
+                    sub_iter,
+                }
+            }
+            Term::Identifier(_) | Term::Terminal(_) | Term::Empty => TermBottomUpIter {
+                node: Some(target),
+                internal: None,
+                sub_iter: [].iter_mut(),
+            },
+        }
+    }
 
-    match term {
-        Term::Concatenation(terms) => {
-            for t in terms {
-                apply_recursively(t, function);
+    fn next_nested(&mut self, internal: &mut TermBottomUpIter<'c>) -> Option<&mut Term> {
+        let mut next = internal.next();
+        
+        if let None = next {
+            if let Some(next_to_iter) = self.sub_iter.next() {
+                *internal = TermBottomUpIter::new(next_to_iter);
+                next = internal.next();
             }
         }
-        Term::Alternation(terms) => {
-            for t in terms {
-                apply_recursively(t, function);
+
+        match next {
+            Some(_) => next,
+            None => self.next_self(),
+        }
+    }
+
+    fn next_self(&mut self) -> Option<&mut Term> {
+        let result = self.node;
+        self.node = None;
+        result
+    }
+}
+
+impl<'c> Iterator for TermBottomUpIter<'c> {
+    type Item = &'c mut Term;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if let None = self.node {
+            return None
+        }
+
+        match &mut self.internal {
+            Some(iter) => self.next_nested(iter),
+            None => self.next_self(),
+        }
+    }
+}
+
+impl Term {
+    // the returned iterator recursively returns all child nodes of this, then this itself.
+    pub fn bottom_up_iter<'c>(&'c mut self) -> TermBottomUpIter<'c> {
+        TermBottomUpIter::new(self)
+    }
+}
+
+// removes all rules from `rules`, by renaming its occurences in other rules using the given mapping
+pub fn remove_rename(rules: RuleStorage, renames: HashMap<RuleId, RuleId>) -> RuleStorage {
+    let mut new_rules = HashMap::new();
+    for (rule_id, mut terms) in rules {
+        if renames.contains_key(&rule_id) {
+            continue;
+        }
+
+        for term in &mut terms {
+            for t in term.bottom_up_iter() {
+                rename_identifier(t, &renames)
             }
         }
-        _ => {}
-    };
+
+        assert!(!terms.is_empty());
+        new_rules.insert(rule_id, terms);
+    }
+
+    new_rules
+}
+
+fn rename_identifier(terminal: &mut Term, renames: &HashMap<RuleId, RuleId>) {
+    match terminal {
+        Term::Identifier(id) if renames.contains_key(id) => {
+            *id = renames.get(id).unwrap().to_owned();
+            // recursively resolve renames, there may be chains of renames in the hashmap
+            rename_identifier(terminal, renames)
+        }
+        _ => {},
+    }
 }
 
 impl Grammar {
